49237669628e919d8689d6f1b81b173200e3136b
hieunv495 <hieunv495@gmail.com>
2017-11-07 06:34:09 +0700

    update db config in deloy settings

--- source/core/deloy_settings.py
+++ source/core/deloy_settings.py
@@ -1,5 +1,16 @@
 from .settings import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smart_water',
+        'USER': 'smart_water',
+        'PASSWORD': 'smart_water12345678',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
